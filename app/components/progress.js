ccc6db3569a87fd9f5d94f4733d6439ba14f53f4
kandaness <kandaness@163.com>
2017-11-04 15:56:26 +0800

    由于使用新的ES6及相关React-Router模块导致部分功能无法实现，现在只能将所有包设置为原来课程的样子，带React-RouterV4之后能更好的实现再说

--- app/components/progress.js
+++ app/components/progress.js
@@ -1,29 +1,24 @@
-import React,{Component} from 'react'
-import './progress.less'
+import React from 'react';
+require('./progress.less');
 
-
-
-class Progress extends React.Component{
-  constructor(){
-    super();
-    this.changeProgress=this.changeProgress.bind(this);
-  }
-  changeProgress(e){
-    let progressBar=this.refs.progressBar;
-    let progress=(e.clientX-progressBar.getBoundingClientRect().left)/progressBar.clientWidth;
-    this.props.onProgressChange && this.props.onProgressChange(progress);
-  }
-  render(){
-    return (
-      <div className="components-progress" ref="progressBar" onClick={this.changeProgress}>
-        <div className="progress" style={{width:`${this.props.progress}%`,background:this.props.barColor}}></div>
-      </div>
-    );
-  }
-};
-
-Progress.defaultProps={
-  barColor:'#2f9842'
-};
+let Progress = React.createClass({
+	getDefaultProps() {
+		return {
+			barColor: '#2f9842'
+		}
+	},
+	changeProgress(e) {
+		let progressBar = this.refs.progressBar;
+		let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+		this.props.onProgressChange && this.props.onProgressChange(progress);
+	},
+    render() {
+        return (
+        	<div className="components-progress" ref="progressBar" onClick={this.changeProgress}>
+        		<div className="progress" style={{width: `${this.props.progress}%`, background: this.props.barColor}}></div>
+        	</div>
+        );
+    }
+});
 
 export default Progress;
--- app/root.js
+++ app/root.js
@@ -1,42 +1,130 @@
-import React,{Component} from 'react'
-import Header from './components/header'
-import {MUSIC_LIST} from './config/musiclist'
-import Player from './page/player'
-import MusicList from './page/musiclist'
+import React, { render } from 'react';
+import { Router, IndexRoute, Link, Route, browserHistory, hashHistory} from 'react-router';
+import { MUSIC_LIST } from './config/config';
+import { randomRange } from './utils/util';
+let PubSub = require('pubsub-js');
 
-class Root extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state={
-      musicList:MUSIC_LIST,
-      currentMusitItem: MUSIC_LIST[0],
-      repeatType: 'cycle'
-    };
-  }
+import PlayerPage from './page/player';
+import listPage from './page/list';
+import Logo from './components/logo'
 
-  componentDidMount(){
-    $('#player').jPlayer({
-      ready:function(){
-        $(this).jPlayer('setMedia',{
-          mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-        }).jPlayer('play');
-      },
-      supplied:'mp3',
-      wmode:'window'
-    });
-  }
 
-  render(){
-    return(
-        <div>
-          <Header/>
-          <MusicList
-            currentMusitItem={this.state.currentMusitItem}
-            musicList={this.state.musicList}
-          ></MusicList>
-        </div>
-    );
-  }
-};
+let App = React.createClass({
+	componentDidMount() {
+		$("#player").jPlayer({
+			supplied: "mp3",
+			wmode: "window",
+			useStateClassSkin: true
+		});
+
+		this.playMusic(this.state.musicList[0]);
+
+		$("#player").bind($.jPlayer.event.ended, (e) => {
+			this.playWhenEnd();
+		});
+		PubSub.subscribe('PLAY_MUSIC', (msg, item) => {
+			this.playMusic(item);
+		});
+		PubSub.subscribe('DEL_MUSIC', (msg, item) => {
+			this.setState({
+				musicList: this.state.musicList.filter((music) => {
+					return music !== item;
+				})
+			});
+		});
+		PubSub.subscribe('PLAY_NEXT', () => {
+			this.playNext();
+		});
+		PubSub.subscribe('PLAY_PREV', () => {
+			this.playNext('prev');
+		});
+		let repeatList = [
+			'cycle',
+			'once',
+			'random'
+		];
+		PubSub.subscribe('CHANAGE_REPEAT', () => {
+			let index = repeatList.indexOf(this.state.repeatType);
+			index = (index + 1) % repeatList.length;
+			this.setState({
+				repeatType: repeatList[index]
+			});
+		});
+	},
+	componentWillUnmount() {
+		PubSub.unsubscribe('PLAY_MUSIC');
+		PubSub.unsubscribe('DEL_MUSIC');
+		PubSub.unsubscribe('CHANAGE_REPEAT');
+		PubSub.unsubscribe('PLAY_NEXT');
+		PubSub.unsubscribe('PLAY_PREV');
+	},
+	getInitialState() {
+		return {
+			musicList: MUSIC_LIST,
+			currentMusitItem: {},
+			repeatType: 'cycle'
+		}
+	},
+	playWhenEnd() {
+		if (this.state.repeatType === 'random') {
+			let index = this.findMusicIndex(this.state.currentMusitItem);
+			let randomIndex = randomRange(0, this.state.musicList.length - 1);
+			while(randomIndex === index) {
+				randomIndex = randomRange(0, this.state.musicList.length - 1);
+			}
+			this.playMusic(this.state.musicList[randomIndex]);
+		} else if (this.state.repeatType === 'once') {
+			this.playMusic(this.state.currentMusitItem);
+		} else {
+			this.playNext();
+		}
+	},
+	playNext(type = 'next') {
+		let index = this.findMusicIndex(this.state.currentMusitItem);
+		if (type === 'next') {
+			index = (index + 1) % this.state.musicList.length;
+		} else {
+			index = (index + this.state.musicList.length - 1) % this.state.musicList.length;
+		}
+		let musicItem = this.state.musicList[index];
+		this.setState({
+			currentMusitItem: musicItem
+		});
+		this.playMusic(musicItem);
+	},
+	findMusicIndex(music) {
+		let index = this.state.musicList.indexOf(music);
+		return Math.max(0, index);
+	},
+	playMusic(item) {
+		$("#player").jPlayer("setMedia", {
+			mp3: item.file
+		}).jPlayer('play');
+		this.setState({
+			currentMusitItem: item
+		});
+	},
+    render() {
+        return (
+            <div className="container">
+            	<Logo></Logo>
+            	{React.cloneElement(this.props.children, this.state)}
+            </div>
+        );
+    }
+});
+
+let Root = React.createClass({
+	render() {
+	    return (
+		    <Router history={hashHistory}>
+		        <Route path="/" component={App}>
+		            <IndexRoute component={PlayerPage}/>
+		            <Route path="/list" component={listPage} />
+		        </Route>
+		    </Router>
+		);
+	}
+});
 
 export default Root;
